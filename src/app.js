dee530764a8fa531ea4ab834e900f8e7f16dc00c
윤주 엄 <yunzoo0915@gmail.com>
2023-04-22 14:30:19 +0900

    order feature set up

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express"); // express 불러오기
 const app = express(); // express 실행
 const userRouter = require("./routes/userRouter"); // user 라우터 불러오기
 const cartRouter = require("./routes/cartRouter");
+const orderRouter = require("./routes/orderRouter");
 
 // JSON 형식의 데이터를 파싱하기 위한 미들웨어
 app.use(express.json());
@@ -14,6 +15,9 @@ app.use("/api/users", userRouter);
 
 // cart 라우터 사용
 app.use("/cart", cartRouter);
+
+// order 라우터 사용
+app.use("/order", orderRouter);
 
 // app 객체 모듈로 내보내기
 module.exports = app;
