6362fbad0b512970bb5471aab6fb83cc4e59a20f
윤주 엄 <yunzoo0915@gmail.com>
2023-04-24 01:32:50 +0900

    app.js api주소오타수정

--- src/db/models/orderModel.js
+++ src/db/models/orderModel.js
@@ -1,32 +1,128 @@
-const { Router } = require("express");
-const router = Router();
-const orderService = require("../services/orderService"); // order 서비스 불러오기
-const loginRequired = require("../middlewares/login-required"); // 로그인 확인 미들웨어 불러오기 (로그인이 필요한 기능이 있을시 해당 라우터에 사용됨)
+const mongoose = require("mongoose");
+const OrderSchema = require("../schemas/order");
 
-// 사용자의 주문 정보 조회 라우터
-router.get("/find-orders", loginRequired, async (req, res) => {
-  try {
-    const orders = await orderService.getOrdersByUserId(req.currentUserId);
-    res.json(orders);
-  } catch (err) {
-    console.log(err);
-    res.status(400).send(`${err}`);
+//Order 모델 생성
+const Order = mongoose.model("Order", OrderSchema);
+
+class OrderModel {
+  // 주문 생성
+  async createOrder(
+    orderItems,
+    shippingAddress,
+    totalPrice,
+    totalDiscount,
+    email = null,
+    userId = null
+  ) {
+    try {
+      // userId 값이 전달되지 않은 경우, 새로운 아이디를 생성하여 추가합니다.
+      if (!userId) {
+        userId = userId || Math.random().toString(36).substring(2, 15);
+      }
+
+      let defaultEmail = "";
+      let defaultShippingAddress = "";
+
+      // 비회원의 경우 이메일과 주소 입력받기
+      if (!email) {
+        email = await this.promptEmail();
+      }
+
+      if (!shippingAddress) {
+        shippingAddress = await this.promptShippingAddress();
+      }
+
+      // userId를 가진 사용자의 주소 정보 조회
+      const user = await User.findOne({ _id: userId });
+      if (user) {
+        defaultShippingAddress = user.address;
+        defaultEmail = user.email;
+      }
+
+      const newOrder = new Order({
+        userId,
+        email: email || defaultEmail,
+        orderItems,
+        shippingAddress: shippingAddress || defaultShippingAddress,
+        totalPrice,
+        totalDiscount,
+        status: "pending", // 주문 상태 초기값 설정
+      });
+
+      await newOrder.save();
+      return newOrder;
+    } catch (error) {
+      throw error;
+    }
   }
-});
 
-// 주문 취소 라우터
-router.post("/find-orders/:orderId/cancel", loginRequired, async (req, res) => {
-  try {
-    const orderId = req.params.orderId;
-    const canceledOrder = await orderService.cancelOrder(
-      orderId,
-      req.currentUserId
-    );
-    res.json({ message: "주문이 취소되었습니다.", order: canceledOrder });
-  } catch (err) {
-    console.log(err);
-    res.status(400).send(`${err}`);
+  //이메일 입력 함수
+  async promptEmail() {
+    const email = prompt("이메일 주소를 입력해주세요.");
+    if (!email) {
+      return this.promptEmail();
+    }
+    return email;
   }
-});
 
-module.exports = router;
+  //배송 주소 입력 함수
+  async promptShippingAddress() {
+    const shippingAddress = prompt("배송 주소를 입력해주세요.");
+    if (!shippingAddress) {
+      return this.promptShippingAddress();
+    }
+    return shippingAddress;
+  }
+
+  // 주문 수정
+  async updateOrder(orderId, status, orderItems, shippingAddress) {
+    try {
+      const order = await Order.findById(orderId);
+      if (order.status !== "processing") {
+        throw new Error("배송이 시작되어 주문 수정이 불가합니다.");
+      }
+      // 상태 업데이트
+      const updatedOrder = await Order.findByIdAndUpdate(
+        orderId,
+        { status },
+        { new: true }
+      );
+
+      // 주문 상품 업데이트
+      if (orderItems) {
+        const updatedOrderItems = [];
+        for (const item of orderItems) {
+          const existingItem = order.orderItems.find(
+            (orderItem) => orderItem.productId.toString() === item.productId
+          );
+          if (existingItem) {
+            // 이미 주문한 상품인 경우 수량 업데이트
+            existingItem.quantity += item.quantity;
+            updatedOrderItems.push(existingItem);
+          } else {
+            // 새로운 상품인 경우 추가
+            updatedOrderItems.push(item);
+          }
+        }
+        updatedOrder.orderItems = updatedOrderItems;
+        await updatedOrder.save();
+      }
+
+      // 배송 주소 업데이트
+      if (shippingAddress) {
+        updatedOrder.shippingAddress = shippingAddress;
+        await updatedOrder.save();
+      }
+
+      return updatedOrder;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+//OrderModel 인스턴스 생성
+const orderModel = new OrderModel();
+
+// orderModel 내보내기
+module.exports = orderModel;
