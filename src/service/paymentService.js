4bc13933c6dc766507e15367fc66f16fa4214872
윤주 엄 <yunzoo0915@gmail.com>
2023-04-23 22:36:00 +0900

    paymentService added

---
+++ src/service/paymentService.js
@@ -0,0 +1,4 @@
+//payment
+const Payment = require("../db/models/paymentModel"); // order 모델 불러오기
+const OrderModel = require("../db/models/orderModel"); // user 모델 불러오기
+const mongoose = require("mongoose");
